6490330c1a93f463e17c6149a4d1a82db043461a
michaeldimmitt <michaelgdimmitt@gmail.com>
2018-05-20 13:40:46 -0400

    Pass in styles as a prop allowing export stateless components to be exported.

--- containers/App.js
+++ containers/App.js
@@ -1,31 +1,29 @@
 import React, {Component} from 'react';
+let hoverStyle, appStyle, tooltipStyle = {}
 
-let hoverStyle = {}
-let appStyle = {}
-let tooltipStyle = {}
-const MouseOverClickHover = ({info, description, clickValues}) => {
+const MouseOverClickHover = ({info, description, clickValues, hoverStyle, tooltipStyle}) => {
 	// console.log(description, clickValues)
 	return (
     <div style={{ minWidth: '160px'}}>
       <a style={hoverStyle}
-	onMouseEnter={()=>info.onMouseEnter()}
-	onMouseLeave={() => info.onMouseLeave(clickValues.hover)}
-	onClick={()=>info.onClick(clickValues)}
+        onMouseEnter={() => info.onMouseEnter()}
+        onMouseLeave={() => info.onMouseLeave(clickValues.hover)}
+        onClick={ () => info.onClick(clickValues)}
       >
- 	{description}
+        {description}
       </a>
       { info.state.wasClicked || info.state.hover ? <div style={tooltipStyle}>Hi There!!! I have been hovered :</div> : <div /> }
     </div>
   )
 }
 
-const NumericHover = ({info, description, hoverVal}) => {
-	// console.log(description, clickValues)
-	return (
+const NumericHover = ({info, description, hoverVal, hoverStyle, tooltipStyle}) => {
+  // console.log(description, clickValues)
+  return (
     <div style={{ minWidth: '160px'}}>
       <a style={hoverStyle}
-	onMouseEnter={()=>info.onMouseMany(hoverVal)}
-	onMouseLeave={()=>info.onMouseLeave()}
+        onMouseEnter={()=>info.onMouseMany(hoverVal)}
+        onMouseLeave={()=>info.onMouseLeave()}
       >
         {description}
       </a>
@@ -33,6 +31,7 @@ const NumericHover = ({info, description, hoverVal}) => {
     </div>
   )
 }
+
 
 export default class App extends Component {
 
@@ -51,7 +50,6 @@ export default class App extends Component {
     })
   }
   onMouseMany = (hoverVal) => {
-  // console.log(hoverVal);
     this.setState({
       hoverVal: hoverVal
     })
@@ -65,7 +63,6 @@ export default class App extends Component {
   }
 
   onClick = (clickValues) => {
-    // console.log(clickValues)
     this.setState({
       hover: !clickValues.hover,
       wasClicked: !clickValues.wasClicked
@@ -85,41 +82,38 @@ export default class App extends Component {
       minHeight: '220px'
     }
     if (this.state.wasClicked || this.state.hover || this.state.hoverVal > 0) {
-      hoverStyle = {
-        backgroundColor: 'blue',
-        color: 'white'
-      }
-      tooltipStyle = {
-        boxShadow: '3px 2px 3px 3px green',
-        width: '150px',
-        margin: '30px auto',
-        borderRadius: '3px',
-        backgroundColor: 'grey'
-      }
+      hoverStyle = {backgroundColor: 'blue',color: 'white'}
+      tooltipStyle = {boxShadow: '3px 2px 3px 3px green',width: '150px',margin: '30px auto',borderRadius: '3px',backgroundColor: 'grey'}
     } else {
-      hoverStyle = {
-        backgroundColor: 'white'
-      }
-      tooltipStyle = {
-        boxShadow: '1px 1px 1px white',
-        width: '150px',
-        margin: '30px auto',
-        borderRadius: '3px'
-      }
+      hoverStyle = {backgroundColor: 'white'}
+      tooltipStyle = {boxShadow: '1px 1px 1px white',width: '150px',margin: '30px auto',borderRadius: '3px'}
     }
+    // console.log('hover:',this.state.hover, 'click:',this.state.wasClicked)
     return (
       <div style={{textAlign:'center'}}>
         <h3 >Tool Tip App</h3>
         <div style={appStyle}>
-          <MouseOverClickHover info={this} description={'Normal Hover Me'							 } clickValues={{hover:this.set(true)  , wasClicked:this.set(false)}}/>
-	  <MouseOverClickHover info={this} description={'Persistent Hover Click(OFF/ON)'} clickValues={{hover:this.state.hover, wasClicked:this.set(false)}}/>
-	  <MouseOverClickHover info={this} description={'Persistent Hover Click(ON/OFF)'} clickValues={{hover:this.state.wasClicked , wasClicked:this.state.wasClicked}}/>
+          <MouseOverClickHover info={this} description={'Normal Hover Me'} clickValues={{hover:this.set(true), wasClicked:this.state.wasClicked}}
+            hoverStyle={hoverStyle} tooltipStyle={tooltipStyle}
+          />
+          <MouseOverClickHover info={this} description={'Persistent Hover Click(OFF/ON)'} clickValues={{hover:this.state.hover, wasClicked:this.set(false)}}
+            hoverStyle={hoverStyle} tooltipStyle={tooltipStyle}
+          />
+          <MouseOverClickHover info={this} description={'Persistent Hover Click(ON/OFF)'} clickValues={{hover:this.state.wasClicked , wasClicked:this.state.wasClicked}}
+            hoverStyle={hoverStyle} tooltipStyle={tooltipStyle}
+          />
         </div>
-	<div style={appStyle}>
-	  < NumericHover info={this} description={'Hover ONE'							 		 } hoverVal={1} />
-	  < NumericHover info={this} description={'Hover ONE and TWO'					 } hoverVal={2} />
-	  < NumericHover info={this} description={'Hover ONE and TWO and THREE'} hoverVal={3} />
-	  </div>
+        <div style={appStyle}>
+          < NumericHover info={this} description={'Hover ONE'} hoverVal={1}
+            hoverStyle={hoverStyle} tooltipStyle={tooltipStyle}
+          />
+          < NumericHover info={this} description={'Hover ONE and TWO'} hoverVal={2}
+            hoverStyle={hoverStyle} tooltipStyle={tooltipStyle}
+          />
+          < NumericHover info={this} description={'Hover ONE and TWO and THREE'} hoverVal={3}
+            hoverStyle={hoverStyle} tooltipStyle={tooltipStyle}
+          />
+        </div>
       </div>
     )
   }
